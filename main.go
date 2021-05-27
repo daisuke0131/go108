d0deff92854d39552372c33f39bd6ce73102414d
daisuke0131 <dai.tachikoma@gmail.com>
2021-05-27 09:16:41 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"strconv"
+)
+
+func main(){
+	fset := token.NewFileSet()
+	f, err := parser.ParseFile(fset,"testdata/a.go",nil,0)
+	if err != nil{
+		panic(err)
+	}
+
+	for _, d := range f.Decls {
+		switch d := d.(type){
+		case *ast.FuncDecl:
+			fmt.Println(d.Name)
+			vars := map[string]string{}
+			for _, b := range d.Body.List {
+				switch b := b.(type){
+				case *ast.AssignStmt:
+					for i, l := range b.Lhs{
+						switch c := l.(type) {
+						case *ast.Ident:
+							r := b.Rhs[i]
+							switch d := r.(type) {
+							case *ast.BasicLit:
+								v, _ := strconv.Unquote(d.Value)
+								if v == "" {
+									vars = map[string]string{
+										c.Name: v,
+									}
+								}
+							case *ast.FuncLit:
+								fmt.Println(d.Body)
+							}
+						}
+					}
+				case *ast.ReturnStmt:
+					for _, e := range b.Results {
+						switch r := e.(type) {
+						case *ast.BasicLit:
+							v, err := strconv.Unquote(r.Value)
+							if err != nil && v == "" {
+								fmt.Println("call empty")
+							}
+						case *ast.Ident:
+							if vars[r.Name] == "" {
+								fmt.Println("call empty")
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
---
+++ testdata/a.go
@@ -0,0 +1,25 @@
+package a
+
+func a_call1(){
+	return
+}
+
+func a_call2() string{
+	return ""
+}
+
+func a_call3() int{
+	return 1
+}
+
+func a_call4() string{
+	a := ""
+	return a
+}
+
+func a_call5() string{
+	a := func() string{
+		return ""
+	}
+	return a()
+}
